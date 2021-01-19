eac1439e9d88c64656822b86b5d7fbf458b5426f
JacksonBey <jacksongrowson@gmail.com>
2021-01-19 15:44:09 -0800

    added info and contacts

--- src/App.js
+++ src/App.js
@@ -31,9 +31,23 @@ function App() {
      <img className='Arrow' src={Arrow} alt='Arrow here'/>
      <h3>About me/ Contact</h3>
      </a>
-     <div id='about' className='aboutDiv'>
-     <h3>About me</h3>
-     <p>Contact Info:</p>
+     <div id='about' className='infoDiv'>
+     <div className='aboutDiv'>
+      <h3>About me</h3>
+      <p>My name is Jackson Beytebiere. Im a software engineer. I graduated from Flatiron coding bootcamp, an intensive 12 week course which involved learning full stack development with modern frameworks
+  .      I have deep knowledge in engineering, physics and biology as I have a bachelors in science from University of Washington.
+        Prior to software development I traveled diverse paths such as a biologist, chef, surveyor and horticulturalist.
+        Those jobs taught me about the ins and out of  handling clients and working in a team.
+        Though at the start of 2020 with the lockdown I had to reevaluate my life.
+        I realized that my true passion was coding. I began with online tutorials and moved my way up to coding competitions
+        which were exhilarating. I am excited to bring my enjoyment of coding together with my strengths of analytical thinking, perseverance and attention to detail to become the best software engineer I can be.
+        </p>
+      </div>
+      <div className='contactDiv'>
+        <h3>Contact Info</h3>
+        <p>Email: jacksongrowson@gmail.com</p>
+        <a href='https://www.linkedin.com/in/jackson-beytebiere-2049951b6/' className='linkedin'>Linkedin</a>
+      </div>
      </div>
      <div id='dummy' className='dummy'></div>
     </div>
