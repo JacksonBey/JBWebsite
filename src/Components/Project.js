293fd8b25724eab449a8b361998357c5bd9d56cd
JacksonBey <jacksongrowson@gmail.com>
2021-01-19 12:56:50 -0800

    switched video player

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-// import VidPlayer  from './VidPlayer'
-import ReactPlayer from 'react-player'
-// import 'semantic-ui-css/semantic.min.css'
+import ReactPlayer from 'react-player';
+import viddemo from '../Project Demos/viddemo.mp4';
 
 
 export default class Project extends Component {
@@ -11,7 +10,25 @@ export default class Project extends Component {
         let {title, description, githubURL, hostedLink, videoSRC} = this.props.project
         return(
             <div className='projectCard'>
-                <ReactPlayer url='https://www.youtube.com/watch?v=v4K8SMMEvRM' width='35vh' height='35vh' className='video'/>
+                {/* <ReactPlayer url='https://www.youtube.com/watch?v=v4K8SMMEvRM' width='35vh' height='35vh' className='video'/> */}
+                <video
+                    id="my-video"
+                    className="video-js"
+                    controls
+                    preload="auto"
+                    width="264"
+                    height="264"
+                    poster="MY_VIDEO_POSTER.jpg"
+                    data-setup="{}">
+                    <source src={viddemo} type="video/mp4" />
+                    <p className="vjs-no-js">
+                    To view this video please enable JavaScript, and consider upgrading to a
+                    web browser that
+                    <a href="https://videojs.com/html5-video-support/" target="_blank"
+                        >supports HTML5 video</a
+                    >
+                    </p>
+                </video>
                 <div className="projectText">
                     <h2>{title}</h2>
                     <p>{description}</p>
